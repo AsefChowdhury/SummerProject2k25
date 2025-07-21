f2edc2bacdcd660184d6faa3efcb7c5dd885db1c
Richu Suresh <richusuresh22@gmail.com>
2025-07-21 22:11:43 +0100

    added back index.css

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route, BrowserRouter } from "react-router";
 import Dashboard from './dashboard/Dashboard';
-import './App.css';
 import MainLayout from "./layouts/MainLayout";
 import SignIn from './authentication/SignIn'
 
