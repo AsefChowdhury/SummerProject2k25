6e39f37765a4cb0f23c612ce604eaa63fbc73eda
Asef Chowdhury <asefch0123@gmail.com>
2025-10-03 23:12:29 +0100

    refactor(toolbar): Move handleClick and executeCommand to ToolbarUtils for modularity

--- frontend/src/components/Toolbar/ToolbarUtils.tsx
+++ frontend/src/components/Toolbar/ToolbarUtils.tsx
@@ -1,7 +1,25 @@
-import { type LexicalEditor, type LexicalCommand, $getSelection, $isRangeSelection } from "lexical";
+import { type LexicalEditor, type LexicalCommand, $getSelection, $isRangeSelection, FORMAT_TEXT_COMMAND } from "lexical";
 
 export type StateAndSetter<T> = {state: T, setter: React.Dispatch<React.SetStateAction<T>>};
-export type ToolbarAnchors = "extendedStyles" | "alignmentOptions";
+export type TextStyles = "Bold" | "Italic"| "Underline" | "Superscript" | "Subscript"| "Code" | "Lowercase" | "Uppercase" | "Strikethrough";
+export type EditorCommand = {payload: string; command: LexicalCommand<string>};
+
+
+export const styleMap: Record<TextStyles, EditorCommand> = {
+    "Bold" : {payload: "bold", command: FORMAT_TEXT_COMMAND},
+    "Italic" : {payload: "italic", command: FORMAT_TEXT_COMMAND},
+    "Underline" : {payload: "underline", command: FORMAT_TEXT_COMMAND},
+    "Superscript" : {payload: "superscript", command: FORMAT_TEXT_COMMAND},
+    "Subscript" : {payload: "subscript", command: FORMAT_TEXT_COMMAND},
+    "Code" : {payload: "code", command: FORMAT_TEXT_COMMAND},
+    "Lowercase" : {payload: "lowercase", command: FORMAT_TEXT_COMMAND},
+    "Uppercase" : {payload: "uppercase", command: FORMAT_TEXT_COMMAND},
+    "Strikethrough" : {payload: "strikethrough", command: FORMAT_TEXT_COMMAND}
+}
+
+export function createDropdownStateMap<T extends string>(map: Record<T, StateAndSetter<null | HTMLElement>>){
+    return map;
+}
 
 export function executeCommand(editor: LexicalEditor, styleObj: {payload: string, command: LexicalCommand<string>}) {
     let {payload, command} = styleObj;
@@ -13,10 +31,10 @@ export function executeCommand(editor: LexicalEditor, styleObj: {payload: string
     })
 }
 
-export function handleClick(
+export function handleClick<T extends string>(
     event: React.MouseEvent<HTMLElement>, 
-    key: ToolbarAnchors,
-    dropdownStateMap:  Record<ToolbarAnchors, StateAndSetter<null | HTMLElement>>
+    key: T,
+    dropdownStateMap:  Record<T, StateAndSetter<null | HTMLElement>>
 ) {
     let {state, setter} = dropdownStateMap[key];
 
