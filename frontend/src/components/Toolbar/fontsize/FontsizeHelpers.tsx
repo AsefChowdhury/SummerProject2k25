2bf15cea420f5f2389f8d203e1b04a0ffef2e406
Asef Chowdhury <asefch0123@gmail.com>
2025-10-07 22:17:49 +0100

    refactor(fontsize): Simplify font size adjustment

--- frontend/src/components/Toolbar/fontsize/FontsizeHelpers.tsx
+++ frontend/src/components/Toolbar/fontsize/FontsizeHelpers.tsx
@@ -1,41 +1,21 @@
-import { $isRangeSelection,$getSelection, type LexicalEditor, $isTextNode } from "lexical";
-
+import { $isRangeSelection,$getSelection, type LexicalEditor } from "lexical";
+import { $patchStyleText } from "@lexical/selection"
 export const MIN_ALLOWED_FONT_SIZE: number =  8;
 export const MAX_ALLOWED_FONT_SIZE: number = 72;
-export const DEFAULT_FONT_SIZE: number = 12;
-
-function formatFontSize(fontSize: number){
-    if ((Number.isNaN(fontSize)) || (fontSize < 1)) return;
-    return fontSize + "px";
-}
-
-function normaliseFontSize(fontSize: number, unit: string){
-    if (unit === 'pt'){
-        return Math.round((fontSize * 4)/3)
-    }
-    return fontSize;
-}
+export const DEFAULT_FONT_SIZE: number = 16;
+export const ACCEPTED_FONT_SIZES: number[] = [8, 9, 10, 11, 12, 14, 16, 18, 24, 30, 36, 48, 60, 72];
+export type FontSizeOptions = number;
 
 function isValidFontSize(fontSize: number): boolean{
-    return fontSize >= MIN_ALLOWED_FONT_SIZE && fontSize <= MAX_ALLOWED_FONT_SIZE;
+    return !isNaN(fontSize) && fontSize >= MIN_ALLOWED_FONT_SIZE && fontSize <= MAX_ALLOWED_FONT_SIZE;
 }
 
 export function adjustFontSize(editor: LexicalEditor, value: number){
+    if (!isValidFontSize(value)) return;
+
     editor.update(() => {
         const selection = $getSelection();
-        if (!$isRangeSelection(selection)) return false;
-
-        const nodes = selection.getNodes();
-        const textNodes = nodes.filter(node => $isTextNode(node));
-        if (!isValidFontSize(value)) return;
-        const normalisedValue = normaliseFontSize(value, "px");
-
-        textNodes.forEach(node => {
-            const fontsize = node.getStyle();
-            console.log("fontsize: ", fontsize);
-        })
-
-        textNodes.forEach(node => node.setStyle(`font-size: ${normalisedValue}px`))
-
+        if (!$isRangeSelection(selection)) return;
+        $patchStyleText(selection, {"font-size": `${value}px`});
     })
 }
