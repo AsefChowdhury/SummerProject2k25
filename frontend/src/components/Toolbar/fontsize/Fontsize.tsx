5109a8fa0dee302cd0c218b94b958a5118aadfbd
Asef Chowdhury <asefch0123@gmail.com>
2025-10-07 22:21:05 +0100

    feat(fontsize): Implement complete font size feature
    Implementation uses mix of buttons and a custom drop-down component to provide flexibility and rigidness

--- frontend/src/components/Toolbar/fontsize/Fontsize.tsx
+++ frontend/src/components/Toolbar/fontsize/Fontsize.tsx
@@ -1,60 +1,68 @@
-import { DEFAULT_FONT_SIZE, MAX_ALLOWED_FONT_SIZE, MIN_ALLOWED_FONT_SIZE, adjustFontSize } from "./FontsizeHelpers";
+import { DEFAULT_FONT_SIZE, MAX_ALLOWED_FONT_SIZE, MIN_ALLOWED_FONT_SIZE, ACCEPTED_FONT_SIZES, adjustFontSize, type FontSizeOptions } from "./FontsizeHelpers";
+import { handleClick, createDropdownStateMap } from "../ToolbarUtils";
+import Dropdown from "../../dropdown/Dropdown";
+import DropdownItem from "../../dropdown/DropdownItem";
 import { type LexicalEditor } from "lexical";
 import { useState, useEffect } from "react";
 
 function Fontsize({ editor }: { editor: LexicalEditor}) {
     const [currentFontSize, setCurrentFontSize] = useState<number>(DEFAULT_FONT_SIZE);
-    const [inputDisplayValue, setInputDisplayValue] = useState<string>(DEFAULT_FONT_SIZE.toString());
-
-    const handleFontsize = (adjustment: "increase" | "decrease" | number) => {
-        setCurrentFontSize(
-            (prev) => {
-                if (adjustment === "increase" && prev < MAX_ALLOWED_FONT_SIZE){
-                    setInputDisplayValue((prev + 1).toString());
-                    return prev + 1;
-                }
-                else if(adjustment === "decrease" && prev > MIN_ALLOWED_FONT_SIZE){
-                    setInputDisplayValue((prev - 1).toString());
-                    return prev - 1;
-                }
-                else if(adjustment !== "increase" && adjustment !== "decrease"){
-                    if (isNaN(adjustment)) return prev;
-                    if (adjustment > MAX_ALLOWED_FONT_SIZE) return MAX_ALLOWED_FONT_SIZE;
-                    if (adjustment < MIN_ALLOWED_FONT_SIZE) return MIN_ALLOWED_FONT_SIZE;
-                    
-                    setInputDisplayValue(adjustment.toString());
-                    return adjustment as number;
-                }
-                return prev;
-            }
-        )
-    }
-
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const value = e.target.value;
-        setInputDisplayValue(value);
-
-        const num = Number(value);
-        if (!isNaN(num)){
-            handleFontsize(num);
-        }
-    }
+    const [fontSizeAnchor, setFontSizeAnchor] = useState<null | HTMLElement>(null);
+    const dropdownStateMap = createDropdownStateMap({
+        "fontSizeOptions" : {state: fontSizeAnchor, setter: setFontSizeAnchor}
+    })
 
     useEffect(() => {
         adjustFontSize(editor, currentFontSize);
     },[editor, currentFontSize]);
 
+    const handleAdjustment = (adjustment: "increase" | "decrease") => {
+        setCurrentFontSize((prev) => {
+            let next = prev;
+
+            if (adjustment === "increase"){
+                next = prev + 1;
+            }
+            else if (adjustment === "decrease"){
+                next = prev - 1;
+            }
+            return Math.min(Math.max(next, MIN_ALLOWED_FONT_SIZE), MAX_ALLOWED_FONT_SIZE);
+        })
+    }
+
+    const handleDropdownSelect = (size: FontSizeOptions) => {
+        setCurrentFontSize(size);
+        dropdownStateMap.fontSizeOptions.setter(null);
+    }
+
+
     return(
         <div className="fontsize-container">
-            <button className="fontsize-decrement" onClick={() => {handleFontsize("decrease")}}>-</button>
+            <button 
+            className="fontsize-decrement" 
+            onClick={() => {handleAdjustment("decrease")}}
+            disabled={currentFontSize <= MIN_ALLOWED_FONT_SIZE}>-</button>
 
-            <input 
-            className="fontsize-input" 
-            value={inputDisplayValue} 
-            onChange={handleInputChange}
-            onBlur={() => setInputDisplayValue(currentFontSize.toString())}/>
-            
-            <button className="fontsize-increment" onClick={() => {handleFontsize("increase")}}>+</button>
+            <button className="font-size-display" onClick={(e) => {handleClick(e, 'fontSizeOptions', dropdownStateMap)}}>{currentFontSize}</button>
+            <Dropdown
+            anchor={dropdownStateMap.fontSizeOptions.state}
+            open={dropdownStateMap.fontSizeOptions.state !== null}
+            onClose={() => {dropdownStateMap.fontSizeOptions.setter(null)}}
+            id="toolbar-dropdown"
+            >
+                {ACCEPTED_FONT_SIZES.map((size) => (
+                    <DropdownItem
+                    key={size}
+                    text={size.toString()}
+                    onClick={() => handleDropdownSelect(size)}
+                    ></DropdownItem>
+                ))}
+            </Dropdown>
+
+            <button 
+            className="fontsize-increment" 
+            onClick={() => {handleAdjustment("increase")}}
+            disabled={currentFontSize >= MAX_ALLOWED_FONT_SIZE}>+</button>
         </div>        
     )
 }
