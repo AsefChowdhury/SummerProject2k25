53c512e24f049d665be152de259c48dcb45fec96
Asef Chowdhury <asefch0123@gmail.com>
2025-11-04 21:16:37 +0000

    feat(Notetaking): Replace InputField component with custom Textarea component. Replace button element with custom Button component

--- frontend/src/components/content-header/ContentHeader.tsx
+++ frontend/src/components/content-header/ContentHeader.tsx
@@ -1,5 +1,5 @@
 import "./ContentHeader.css";
-import InputField from "../input-field/InputField";
+import Textarea from "../textarea/Textarea";
 import ContentSave from "../content-save/ContentSave";
 import { useLexicalComposerContext } from "@lexical/react/LexicalComposerContext";
 import { type NotePayload } from "../../notes/NoteUtils";
@@ -17,8 +17,11 @@ function ContentHeader({ title, onTitleChange, id, onSave} : ContentHeaderProps)
     return(
         <div className="header-container">
             <div className="file-name-container">
-                <p>File name:</p>
-                <InputField value={title} onChange={(e) => onTitleChange(e.target.value)}/>
+                <Textarea 
+                className="note-title"
+                value={title} 
+                onChange={(e) => onTitleChange(e.target.value)}
+                maxLength={100}/>
             </div>
             <div className="file-operations">
                 <ContentSave editor={editor} title={title} id={id} onSave={onSave}/>
--- frontend/src/components/content-save/ContentSave.tsx
+++ frontend/src/components/content-save/ContentSave.tsx
@@ -1,6 +1,7 @@
-import IconButton from "../icon-button/IconButton";
 import { editorToJSON, editorStateToJSON, type NotePayload} from "../../notes/NoteUtils";
 import { type LexicalEditor } from "lexical";
+import Button from "../button/Button";
+import { Save } from "lucide-react";
 
 type ContentSaveProps = {
     editor : LexicalEditor;
@@ -23,7 +24,11 @@ function ContentSave({ editor, title, id, onSave } : ContentSaveProps) {
     }
 
     return(
-        <button className="save-button" onClick={handleSave}>Save</button>
+        <Button 
+        iconLeft={Save}
+        text="Save"
+        variant="outlined"
+        onClick={handleSave}/>
     )
 }
 
