683e15aaed62290e3d420ab4136272426a34254f
Asef Chowdhury <asefch0123@gmail.com>
2025-11-07 21:42:38 +0000

    feat(List formatting): Implement icons via LIST_ICON map

--- frontend/src/components/Toolbar/list-formatting/ListFormatting.tsx
+++ frontend/src/components/Toolbar/list-formatting/ListFormatting.tsx
@@ -1,13 +1,19 @@
+import "./ListFormatting.css"
 import { useState } from "react";
-import type { ListType } from "./ListFormattingHelpers";
-import { toggleListFormat } from "./ListFormattingHelpers";
+import { type ListType, toggleListFormat} from "./ListFormattingHelpers";
 import type { LexicalEditor } from "lexical";
+import { ListBulletsIcon, ListNumbersIcon } from "@phosphor-icons/react";
 
 type ListFormats = "Bulleted List" | "Numbered List";
 
 const listTypeMap = {
     "Bulleted List" : "bullet",
     "Numbered List" : "number"
+}
+
+const LIST_ICON: Record<ListFormats, React.ReactNode> = {
+    "Bulleted List": <ListBulletsIcon size={22}/>,
+    "Numbered List": <ListNumbersIcon size={22}/>
 }
 
 function ListFormatting({ editor }: {editor : LexicalEditor}) {
@@ -20,11 +26,11 @@ function ListFormatting({ editor }: {editor : LexicalEditor}) {
             {listFormats.map(listFormat => (
                 <button
                 key={listFormat}
-                className={`format-button ${activeFormat.includes(listFormat) ? "active" : ""}`}
+                className={`list-format-button ${activeFormat.includes(listFormat) ? "active" : ""}`}
                 onClick={() => {
                     toggleListFormat(editor, listTypeMap[listFormat] as ListType)
                 }}
-                >{listFormat}</button>
+                >{LIST_ICON[listFormat]}</button>
             ))}
         </div>
     )
