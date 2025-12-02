08e3b88a80cfff1ff7876a2beadcf0efa3a413e6
Asef Chowdhury <asefch0123@gmail.com>
2025-12-02 20:43:40 +0000

    feat(notes): Implement note list page layout and NoteCard component structure

--- frontend/src/notes/MyNotes.tsx
+++ frontend/src/notes/MyNotes.tsx
@@ -1,7 +1,41 @@
+import Card from "../components/card/Card";
+import api from "../api";
+import Modal from "../components/modal/Modal";
+import Button from "../components/button/Button";
+import { useState, useEffect } from "react";
+
+
+type NoteCardProps = {
+    title : string,
+    preview : string,
+    id : number,
+    onDelete : (id: number) => void,
+}
+
+function NoteCard(props: NoteCardProps){
+    return(
+        <Card>
+            <div className="note-card-container"></div>
+        </Card>
+    )
+}
+
 function MyNotes() {
     return (
-        <div>
-            <h1>My Notes</h1>
+        <div className="note-list-container">
+            <div className="recent-notes">
+                <h1>Recent Notes</h1>
+            </div>
+
+            <div className="user-notes">
+                <div className="note-list-header">
+                    <h1>My Notes</h1>
+                </div>
+
+                <div className="user-notes-list">
+                </div>
+            </div>
+
         </div>
     )
 }
