1e7808c1cd03b0449b8a07e9447b013c78328987
Asef Chowdhury <asefch0123@gmail.com>
2025-12-04 09:46:07 +0000

    feat(NoteUtils): Implement helper function to extract plain text from Lexical JSON
    This helper function's purpose is to assist in providing a preview of a users' note. It creates a temporary editor, parses the JSON argument which is converted to a Lexical EditorState object, gets to the root of the Lexical node tree and grabs the text content before returning it

--- frontend/src/notes/NoteUtils.tsx
+++ frontend/src/notes/NoteUtils.tsx
@@ -1,4 +1,6 @@
-import type { EditorState, LexicalEditor } from "lexical";
+import { $getRoot, createEditor, type EditorState, type LexicalEditor } from "lexical";
+import { ListItemNode, ListNode } from "@lexical/list";
+import { HeadingNode, QuoteNode } from "@lexical/rich-text"
 import api from "../api";
 
 export interface NotePayload {
@@ -7,6 +9,20 @@ export interface NotePayload {
     note_content : string;
     author ?: number;
     updated_at : string
+}
+
+const tempConfig = {
+    namespace: 'PreviewEditor',
+    onError: (error : Error) => {console.log(error)},
+
+    nodes: [
+        ListNode,
+        ListItemNode,
+        HeadingNode,
+        QuoteNode
+    ],
+
+    theme: {}
 }
 
 export function editorToJSON(editor : LexicalEditor) {
@@ -26,5 +42,25 @@ export async function loadNote(noteId: string): Promise<NotePayload | null>{
     } catch (error) {
         console.log("Failed to load note from API: ", error);
         return null;
+    }
+}
+
+export function extractPlainTextFromJSON(jsonState: string | null | undefined): string {
+    if(!jsonState) return '';
+
+    try {
+        const editor = createEditor(tempConfig);
+        const editorState = editor.parseEditorState(jsonState);
+
+        let plainText = '';
+
+        editorState.read(() => {
+            plainText = $getRoot().getTextContent();
+        })
+
+        return plainText;
+    } catch (error) {
+        console.log("Failed to parse or read lexical JSON for preview", error);
+        return '';
     }
 }
