9f58fb30ccece7ad8946eb3f185f4eb640a6414c
Asef Chowdhury <asefch0123@gmail.com>
2025-12-04 08:02:55 +0000

    feat(MyNotes): Add NoteCard component and integrate API for fetching and deleting user notes

--- frontend/src/notes/MyNotes.tsx
+++ frontend/src/notes/MyNotes.tsx
@@ -1,9 +1,13 @@
+import "./note-styles/MyNotes.css";
+import { type NotePayload } from "./NoteUtils";
+import IconButton from "../components/icon-button/IconButton";
+import Button from "../components/button/Button";
+import Modal from "../components/modal/Modal";
+import { useState, useEffect } from "react";
 import Card from "../components/card/Card";
 import api from "../api";
-import Modal from "../components/modal/Modal";
-import Button from "../components/button/Button";
-import { useState, useEffect } from "react";
 
+import { PencilLineIcon, TrashIcon } from "@phosphor-icons/react";
 
 type NoteCardProps = {
     title : string,
@@ -14,26 +18,113 @@ type NoteCardProps = {
 }
 
 function NoteCard(props: NoteCardProps){
+    const formattedDate = props.lastModified.toLocaleString("en-GB", {
+        year : 'numeric',
+        month : 'short',
+        day : 'numeric',
+        hour : '2-digit',
+        minute : '2-digit'
+    });
+
     return(
-        <Card>
-            <div className="note-card-container"></div>
+        <Card className="note-card-container">
+            <p>{props.title}</p>
+
+            <div className="note-content-preview">
+                {props.preview}
+            </div>
+
+            <div className="note-last-modified">
+                Last Modified: {formattedDate}
+            </div>
+            
+            <div className="note-actions">
+                <Button text="Preview" variant="outlined" to={`/notes/preview/${props.id}/`}/>
+                <IconButton icon={PencilLineIcon} to={`/notes/edit/${props.id}/`} tooltip="Edit Note"/>
+                <IconButton icon={TrashIcon} onClick={() => {props.onDelete(props.id)}} tooltip="Delete Note"/>
+            </div>
         </Card>
     )
 }
 
+const createPreview = (content: string | null | undefined, contentType: "title" | "note content"): string => {
+    
+    let maxLength: number;
+    if (contentType === "title") {
+        maxLength = 20;
+    }
+    else{
+        maxLength = 100;
+    }
+
+    const textContent = content ? content.trim() : '';
+
+    if (textContent.length === 0) {
+        if (contentType === "note content") {
+            return 'No content has been added to this note yet.'            
+        }
+        return '';
+    }
+
+
+    const previewText = textContent.substring(0, maxLength);
+    return previewText + (textContent.length > maxLength ? '...' : '');
+}
+
 function MyNotes() {
+    const [notes, setNotes] = useState<NotePayload[]>([]);
+    const [showDeleteModal, setShowDeleteModal] = useState(false);
+    const [noteToDelete, setNoteToDelete] = useState({id: 0, title: undefined});
+    const recentNotesList = [...notes].slice(0, 1);
+
+    const getNotes = async () => {
+        await api
+        .get('api/notes/')
+        .then(response => {
+            setNotes(response.data);
+        })
+        .catch(error => console.log(error));
+    };
+
+    const handleDelete = async (id: number) => {
+        await api.delete(`api/notes/${id}/`).catch(error => console.log(error));
+        getNotes();
+    };
+
+    useEffect(() => {
+        getNotes();
+    }, []);
+
     return (
         <div className="note-list-container">
+
             <div className="recent-notes">
-                <h1>Recent Notes</h1>
+                <div className="recent-notes-header">
+                    <h1>Recent Notes</h1>
+                </div>
+
+                <div className="recent-notes-list">
+                    {recentNotesList.map(note => (
+                        <NoteCard
+                            key={note.id as string}
+                            id={Number(note.id)}
+                            title={createPreview(note.note_title, "title")}
+                            preview={createPreview(note.note_content, "note content")}
+                            lastModified={new Date(note.updated_at || 0)}
+                            onDelete={handleDelete}
+                        />
+                    ))}
+                </div>
+
             </div>
 
             <div className="user-notes">
-                <div className="note-list-header">
+                <div className="user-notes-header">
                     <h1>My Notes</h1>
                 </div>
 
                 <div className="user-notes-list">
+
                 </div>
             </div>
 
