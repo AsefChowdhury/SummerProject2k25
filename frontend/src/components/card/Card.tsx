9fd0196057a33e083d8c52a1f7a9d3fe9f50611a
Asef Chowdhury <asefch0123@gmail.com>
2025-12-09 18:56:08 +0000

    feat(card): Add onClick prop

--- frontend/src/components/card/Card.tsx
+++ frontend/src/components/card/Card.tsx
@@ -4,11 +4,12 @@ type CardProps = {
     children: React.ReactNode
     className?: string
     style?: React.CSSProperties
+    onClick ?: React.MouseEventHandler<HTMLElement>
 }
 
 function Card(props: CardProps) {
     return(
-        <div className={`card-container ${props.className ?? ''}`}>
+        <div className={`card-container ${props.className ?? ''}`} onClick={props.onClick}>
             {props.children}
         </div>
     )
