0824481e38d6d9f3e101578ae6895f7a3844bdaf
Richu Suresh <richusuresh22@gmail.com>
2026-01-20 17:47:50 +0000

    feat: Removed containers in custom button components as they were redundant

--- frontend/src/components/IconButton/IconButton.tsx
+++ frontend/src/components/IconButton/IconButton.tsx
@@ -10,24 +10,20 @@ type IconButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
 
 function IconButton({icon: Icon, className, ...props}: IconButtonProps) {
     const content = (
-        <div className={`icon-button-container ${className ?? ''}`}>
-            <button {...props} className="button" disabled={props.disabled}>
-                <Icon className="icon-image" fill='currentColor'/>
-                {props.tooltip && <span className="tooltiptext">{props.tooltip}</span>}
-            </button>
-        </div>
+        <button {...props} className={`icon-button ${className ?? ''}`} disabled={props.disabled}>
+            <Icon className="icon-image" fill='currentColor'/>
+            {props.tooltip && <span className="tooltiptext">{props.tooltip}</span>}
+        </button>
     )
 
     if(props.to){
         return(
-            <div className="icon-button-container">
-                <Link to={props.to}>
-                    <button {...props} className='button'>
-                        <Icon className="icon-image" fill='currentColor'/>
-                        {props.tooltip && <span className="tooltiptext">{props.tooltip}</span>}
-                    </button>
-                </Link>
-            </div>
+            <Link to={props.to}>
+                <button {...props} className={`icon-button ${className ?? ''}`} disabled={props.disabled}>
+                    <Icon className="icon-image" fill='currentColor'/>
+                    {props.tooltip && <span className="tooltiptext">{props.tooltip}</span>}
+                </button>
+            </Link>
         )
     }
     return content
