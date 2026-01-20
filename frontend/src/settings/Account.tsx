098ccd077b7316b852f14397fdd1093e3d111a4d
Richu Suresh <richusuresh22@gmail.com>
2026-01-20 17:48:15 +0000

    feat: Created form elements for account page in settings

--- frontend/src/settings/Account.tsx
+++ frontend/src/settings/Account.tsx
@@ -1,6 +1,19 @@
+import Button from "../components/button/Button"
+import InputField from "../components/input-field/InputField"
+import "./settings-styles/Account.css"
+
 function Account(){
     return(
-        <h1>Account</h1>
+        <div className="account-page">
+            <h1 className="page-header">Account</h1>
+            <form className="account-form">
+                <InputField label={{text: "Username", for: "username"}} placeholder="Username" id="username" variant="outlined" />
+                <InputField label={{text: "Email", for: "email"}} placeholder="Email" id="email" variant="outlined" />
+                <Button type="submit" id="save-button" text="Save changes"/>
+            </form>
+            <h2 className="page-header delete-account">Delete Account</h2>
+            <Button id="delete-account-button" text="Delete my account" variant="outlined"/>
+        </div>
     )
 }
 
